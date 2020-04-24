6fb39584e5db663e1dba30bf95548b072ea02d0e
s-jaiganesh <64076063+s-jaiganesh@users.noreply.github.com>
2020-04-24 17:09:01 +0530

    Update aix_precheck.py

--- aix_precheck.py
+++ aix_precheck.py
@@ -5,9 +5,9 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 ANSIBLE_METADATA = {
-    'metadata_version': '1.0',
-    'status': ['preview'],
-    'supported_by': 'none'
+    'metadata_version': '1.0',
+    'status': ['preview'],
+    'supported_by': 'none'
 }
 
 DOCUMENTATION = r'''
@@ -20,18 +20,18 @@ description:
 - This module creates boot image on disks
 version_added: '1.0'
 options:
-  name:
-    description:
-    - name of action example, bosboot, bootlist
-    type: str
-    default: yes
+  name:
+    description:
+    - name of action example, bosboot, bootlist
+    type: str
+    default: yes
 '''
 
 EXAMPLES = r'''
 - name: Create a volume group datavg
-  aix_precheck:
-    name: bosboot
-    
+  aix_precheck:
+    name: bosboot
+    
 '''
 
 RETURN = r''' # '''
@@ -40,53 +40,57 @@ from ansible.module_utils.basic import AnsibleModule
 
 def bosboot(module):
 
-    # Define pvs_to_run_bosboot (list of physical volumes to run bosboot).
-    lslv_cmd = module.get_bin_path('lslv', True)
-    rc, hd5_pvs, err = module.run_command("%s -l '%s'" % (lslv_cmd, 'hd5'))
-    if rc != 0:
-        module.fail_json(msg="Failing to execute '%s' command." % lslv_cmd)
+    # Define pvs_to_run_bosboot (list of physical volumes to run bosboot).
+    lslv_cmd = module.get_bin_path('lslv', True)
+    rc, hd5_pvs, err = module.run_command("%s -l '%s'" % (lslv_cmd, 'hd5'))
+    if rc != 0:
+        module.fail_json(msg="Failing to execute '%s' command." % lslv_cmd)
 
-    pvs_to_run_bosboot = []
-    for line in hd5_pvs.splitlines()[2:]:
-        pvs_to_run_bosboot.append(line.split()[0])   
-    
-    if len(pvs_to_run_bosboot) <= 0:
-        changed = False
-        bosboot_msg = "No Boot image present on any PV"
-        return changed, msg
-    elif len(pvs_to_run_bosboot) == 2:
-        bosboot_cmd = module.get_bin_path('bosboot', True)
-        rc, stdout, stderr = module.run_command("%s -ad %s" % (bosboot_cmd, '/dev/'.join(pvs_to_run_bosboot)))
-        if rc != 0:
-            changed = False
-            module.fail_json(msg="Unable to run bosboot '%s' '%s' '%s'" % rc=rc, stdout=stdout, stderr=stderr)
-        else:
-            changed = True
-            bosboot_msg = "Bosboot executed on '%s'"
-    elif len(pvs_to_run_bosboot) == 1:
-        bosboot_cmd = module.get_bin_path('bosboot', True)
-        rc, stdout, stderr = module.run_command("%s -ad %s" % (bosboot_cmd, '/dev/'.join(pvs_to_run_bosboot)))
-        if rc != 0:
-            changed = False
-            module.fail_json(msg="Unable to run bosboot '%s' '%s' '%s'" % rc=rc, stdout=stdout, stderr=stderr)
-        else:
-            changed = True
-            bosboot_msg = "Bosboot executed on '%s'"
-    else:
-    msg = bosboot_msg
-    return changed, msg
-    
-    
+    pvs_to_run_bosboot = []
+    for line in hd5_pvs.splitlines()[2:]:
+        pvs_to_run_bosboot.append(line.split()[0])   
+    
+    if len(pvs_to_run_bosboot) <= 0:
+        changed = False
+        bosboot_msg = "No Boot image present on any PV"
+        return changed, msg
+    elif len(pvs_to_run_bosboot) == 2:
+        bosboot_cmd = module.get_bin_path('bosboot', True)
+        rc, stdout, stderr = module.run_command("%s -ad %s" % (bosboot_cmd, '/dev/'.join(pvs_to_run_bosboot)))
+        if rc != 0:
+            changed = False
+            module.fail_json(msg="Unable to run bosboot")
+        else:
+            changed = True
+            bosboot_msg = "Bosboot executed"
+    elif len(pvs_to_run_bosboot) == 1:
+        bosboot_cmd = module.get_bin_path('bosboot', True)
+        rc, stdout, stderr = module.run_command("%s -ad %s" % (bosboot_cmd, '/dev/'.join(pvs_to_run_bosboot)))
+        if rc != 0:
+            changed = False
+            module.fail_json(msg="Unable to run bosboot") 
+        else:
+            changed = True
+            bosboot_msg = "Bosboot executed"
+    else:
+        changed = False
+        bosboot_msg = "unknown failure"
+        module.fail_json(msg="Unable to run bosboot")
+
+    msg = bosboot_msg
+    return changed, msg
+    
+    
 def main():
-    module = AnsibleModule(
-        argument_spec=dict(
-            name=dict(type='str', required=True),
-        ),
-        supports_check_mode=True,
-    )
+    module = AnsibleModule(
+        argument_spec=dict(
+            name=dict(type='str', required=True),
+        ),
+        supports_check_mode=True,
+    )
 
-    name = module.params['name']    
-    changed, msg = bosboot(module)
+    name = module.params['name']    
+    changed, msg = bosboot(module)
 
 if __name__ == '__main__':
-    main()
+    main()
