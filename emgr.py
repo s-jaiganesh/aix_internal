d4193c457e869c03aba1c49abe3f450544242a38
s-jaiganesh <64076063+s-jaiganesh@users.noreply.github.com>
2020-05-27 21:14:29 +0530

    Update emgr.py

--- emgr.py
+++ emgr.py
@@ -52,17 +52,23 @@ def _remove_all_efix_pkg_preview(module):
         else:
             res = True
 
-    if res is True:        
+    if res is True:
+        if preview is True:            
+            emgr_remove_cmd = ("%s -p -r" % (emgr_cmd))
+        elif preview is False:            
+            emgr_remove_cmd = ("%s -r" % (emgr_cmd))
+        else:
+            module.fail_json(msg="Preview option not avaiable. valid must be True or False")
+            
         lines = lables.split('\n',2)[-1]
-        for line in lines.splitlines(): 
-             fields = line.strip().split()        
+        for line in lines.splitlines():                   
+             fields = line.strip().split()
              _to_remove.append(fields[2])
              
-        result = { 'lables_to_remove' : _to_remove}
         _to_remove_count = len(_to_remove)
         if len(_to_remove) > 0:        
             for lable in _to_remove:
-                rc, out, err = module.run_command("%s -p -r %s" % (emgr_cmd, lable))
+                rc, out, err = module.run_command("%s %s" % (emgr_remove_cmd, lable))
                 if rc is 0:
                      out = out.rstrip(b"\r\n")
                      _success.append(lable)
@@ -70,26 +76,20 @@ def _remove_all_efix_pkg_preview(module):
                 if rc != 0:
                     err = err.rstrip(b"\r\n")
                     _failed.append(lable)
-                    _err.append(err)
-                     
-            if len(_success) > 0:
-                result = { 'changed' : 'True'}
-                      
-            result = { 'lables_failed' : _failed}
-            result = { 'lables_success' : _success}
-            result = { 'stdout' : _out}
-            result = { 'stderr' : _err}
+                    _err.append(err)                                             
 
             if len(_failed) != 0:
                 if _to_remove_count == len(_failed):
-                    result = { 'changed' : 'False'}
-                    result = { 'msg' : 'PREVIEW none of the EFIX are affected'}
+                    result = { 'lables_to_remove' : _to_remove, 'lables_failed' : _failed, 'lables_success' : _success, 'stdout' : _out, 'stdout_lines' : _out, 'stderr' : _err, 'stderr_lines' : _out, 'changed' : False, 'msg' : 'PREVIEW none of the EFIX are affected' }
+                    result.update(result)
                     module.fail_json(**result)
                 else:
-                    result = { 'msg' : 'PREVIEW failed to affect one or more EFIX'}
-                    module.exit_json(**result)
+                    result = { 'lables_to_remove' : _to_remove, 'lables_failed' : _failed, 'lables_success' : _success, 'stdout' : _out, 'stdout_lines' : _out, 'stderr' : _err, 'stderr_lines' : _out, 'changed' : True, 'msg' : 'PREVIEW failed to affect one or more EFIX' }
+                    result.update(result)
+                    module.fail_json(**result)
             elif len(_failed) is 0:
-                result = { 'msg' : 'PREVIEW success for all EFIX'}
+                result = { 'lables_to_remove' : _to_remove, 'lables_failed' : _failed, 'lables_success' : _success, 'stdout' : _out, 'stdout_lines' : _out, 'stderr' : _err, 'stderr_lines' : _out, 'changed' : True, 'msg' : 'PREVIEW success for all EFIX' }
+                result.update(result)
                 module.exit_json(**result)            
             else:
                 module.exit_json(msg="Empty arrary")
