3d9ca5ea2e0c3ac6325474315466dc20dc8db92d
s-jaiganesh <64076063+s-jaiganesh@users.noreply.github.com>
2020-06-04 17:58:22 +0530

    Update devices.py

--- devices.py
+++ devices.py
@@ -9,31 +9,58 @@ ANSIBLE_METADATA = {
     'status': ['preview'],
     'supported_by': 'none'
 }
+DOCUMENTATION = '''
+---
+module: aix_devices
+short_description: lsdev command module
+
+version_added: "1.0"
+
+description:
+    - "This is a custom module for lsdev -C command"
+
+author:
+    - Jai Ganesh
+'''
+
+EXAMPLES = '''
+- name: List disks from Lsdev command
+  aix_devices:
+    name: disk
+
+- name: List fcs from Lsdev command
+  aix_devices:
+    name: fcs    
+
+- name: List ethernet from Lsdev command
+  aix_devices:
+    name: ent    
+
+- name: List eth channel from Lsdev command
+  aix_devices:
+    name: port
+'''
 
 from ansible.module_utils.basic import AnsibleModule
 
-def _list_devices(module, device):
+def _list_devices(module, _device):
 
     lsdev_cmd = module.get_bin_path('lsdev', True)
     rc, out, err = module.run_command("%s '-C'" % (lsdev_cmd)
-    
-    if rc != 0:
-        changed = False
-        module.fail_json(msg="Failing to execute '%s' command." % lsdev_cmd)
-    else:
-        hdisks = []
-        output = []
+    if rc is 0:        
+        _devices = []
         for line in out.splitlines():
-            if 'device' in line:
-                fields = line.strip().split()
-                out = line
-                hdisks.append(fields[0])
-                output.append(line)
-        result = dict(
-            output=output,
-            rc=rc,
-            changed=True,
-        )
+            if _device in line:
+                _devices.append(line)
+            if len(_devices) is 0:
+                result = { 'stdout':'', 'stderr':'', 'rc':rc, 'changed':False, 'msg': "Device not found" }
+                module.exit_json(**result)
+            else:
+                result = { 'stdout':_devices, 'stderr':'', 'rc':rc, 'changed':False, 'msg': "command successful" }
+                module.exit_json(**result)
+                
+    if rc != 0:
+        result = { 'stdout': '', 'stderr':out, 'rc':rc, 'changed':False, 'msg': "Failed to execute the command" }
         module.exit_json(**result)
 
 def main():
@@ -43,13 +70,14 @@ def main():
         ),
         supports_check_mode=True,
     )
-    if module.param['name'] == 'disk':
+    name = module.params['name']
+    if name == 'disk':
         _list_devices(module, 'hdisk')
-    elif module.param['name'] == 'fcs':
+    elif name == 'fcs':
         _list_devices(module, 'fcs')
-    elif module.param['name'] == 'ent':
+    elif name == 'ent':
         _list_devices(module, 'ent')
-    elif module.param['name'] == 'port':
+    elif name == 'port':
         _list_devices(module, 'EtherChannel')
     else
         module.fail_json(msg="Invalid input")
