8aeaea37c770efa300eb0281a537589fef6eb0b1
s-jaiganesh <64076063+s-jaiganesh@users.noreply.github.com>
2020-12-08 17:27:36 +0530

    Update ibm_chfs.py

--- ibm_chfs.py
+++ ibm_chfs.py
@@ -5,12 +5,24 @@ EXAMPLES = r'''
   ibm_chfs:
     size: 512
     fs_name: /var
-       
+    action: increase
+
+- name: FS decrease command in MB
+  ibm_chfs:
+    size: 512
+    fs_name: /var
+    action: decrease       
 '''
-def _chfs(module, fs_size, fs_name):
+
+def _chfs(module, fs_size, fs_name, action):
     chfs_cmd = module.get_bin_path('chfs', True)
-    rc, chfs_out, err = module.run_command("%s -a size=%s %s" % (chfs_cmd, fs_size, fs_name))
-    
+    if 'increase' in action:
+        rc, chfs_out, err = module.run_command("%s -a size=+%sM %s" % (chfs_cmd, fs_size, fs_name))
+    elif 'decrease' in action:
+        rc, chfs_out, err = module.run_command("%s -a size=-%sM %s" % (chfs_cmd, fs_size, fs_name))
+    else
+        module.fail_json(msg='Unknown option. Please choose increase/decrease')
+
     if rc == 0:
         result = { 'stdout' : chfs_out, 'rc' : rc, 'stderr': err, 'changed' : True, 'msg' : 'the chfs command completed successfully' }
  
@@ -25,14 +37,16 @@ def main():
         argument_spec=dict(
             size=dict(type='int', required=True),
             fs_name=dict(type='str', required=True),
+            action=dict(type='str', required=True),
 
         ),
         supports_check_mode=True,
     )
     fs_size = module.params['size']
     fs_name = module.params['fs_name']
+    action = module.params['action']
 
-    _chfs(module, fs_size, fs_name)
+    _chfs(module, fs_size, fs_name, action)
 
 if __name__ == '__main__':
     main()
